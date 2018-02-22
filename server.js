cc904ed748053ec56af67a74078701cd338b546b
woravich-k <woravich@hotmail.com>
2018-02-22 09:22:15 -0500

    simple https server

---
+++ server.js
@@ -0,0 +1,18 @@
+//express is the server that forms part of the nodejs program
+var express = require('express');
+var app = express();
+
+var https = require('https');
+var fs = require('fs');
+var privateKey = fs.readFileSync('/home/studentuser/certs/client-key.pem').toString();
+var certificate = fs.readFileSync('/home/studentuser/certs/client-cert.pem').toString();
+var credentials = {key: privateKey, cert: certificate};
+var httpsServer = https.createServer(credentials,app);
+httpsServer.listen(4443);
+
+app.get('/',function(req,res){
+	//run some server side code
+	console.log('the server has recieved a request');
+	res.send('Hello World');
+});
+
