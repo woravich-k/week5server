ff08f7140858c71df5cc0b7d37a3591aa17c543e
woravich-k <woravich@hotmail.com>
2018-02-22 10:02:15 -0500

    serve various filenames and directories

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 
+
 var https = require('https');
 var fs = require('fs');
 var privateKey = fs.readFileSync('/home/studentuser/certs/client-key.pem').toString();
@@ -10,10 +11,8 @@ var credentials = {key: privateKey, cert: certificate};
 var httpsServer = https.createServer(credentials,app);
 httpsServer.listen(4443);
 
-app.get('/:fileName',function(req,res){
-	//run some server side code
-	var fileName =req.params.fileName;
-	console.log(fileName +' requested');
-	res.sendFile(__dirname + '/' + fileName);
-});
+
+app.use(express.static(__dirname))
+	
+
 
